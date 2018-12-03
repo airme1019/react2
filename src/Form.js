e6f40e100d725c9a4f1ad92f3d5293131d46a203
Ling Zhao <lingzhao@apples-iMac.local>
2018-12-02 23:51:11 -0800

    Creating table, form, api components

---
+++ src/Form.js
@@ -0,0 +1,35 @@
+import React, {Component} from 'react';
+
+class Form extends Component {
+  constructor(props) {
+    super(props);
+    this.initialState = {
+      name: '',
+      job: ''
+    };
+    this.state = this.initialState;
+  }
+  handleChange = event => {
+    const {name, value} = event.target;
+    this.setState({
+      [name]: value
+    });
+  }
+  submitForm = () => {
+    this.props.handleSubmit(this.state);
+    this.setState(this.initialState);
+  }
+  render(){
+    const { name, job } = this.state;
+    return (
+    <form >
+    <label>Name</label>
+    <input type="text" name="name" value={name} onChange={this.handleChange} />
+    <label>Job</label>
+    <input type="text" name="job" calue={job} onChange={this.handleChange} />
+    <input type="button" value="Submit" onClick={this.submitForm} />
+    </form>
+  );
+  }
+}
+export default Form;
---
+++ src/List.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class List extends Component {
+  render (){
+    return (
+      <div>
+        <ul>
+          <li>Item 1</li>
+          <li>Item 2</li>
+        </ul>
+      </div>
+    )
+  };
+}
+export default List;
